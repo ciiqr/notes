f4f9b0f8dbc75e62909293fcec0a269d4dbb7c1f
William Villeneuve <ciiqr@users.noreply.github.com>
2019-09-03 17:54:32 -0400

    added initial desktop support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:notes/screens/note_editor_screen.dart';
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride;
 
-void main() => runApp(MyApp());
+/// If the current platform is desktop, override the default platform to
+/// a supported platform (iOS for macOS, Android for Linux and Windows).
+/// Otherwise, do nothing.
+void _setTargetPlatformForDesktop() {
+  TargetPlatform targetPlatform;
+  if (Platform.isMacOS) {
+    targetPlatform = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    targetPlatform = TargetPlatform.android;
+  }
+  if (targetPlatform != null) {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
+}
+
+void main() {
+  _setTargetPlatformForDesktop();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
